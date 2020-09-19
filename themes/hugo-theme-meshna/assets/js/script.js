4a8fd49474fc9809d16e755b90f2046bd433f026
Benjamin Kostenbader <benjamin.kostenbader@gmail.com>
2020-09-18 21:44:09 -0400

    a contact form for the homepage

--- themes/hugo-theme-meshna/assets/js/script.js
+++ themes/hugo-theme-meshna/assets/js/script.js
@@ -311,3 +311,54 @@ jQuery(function ($) {
   });
 
 });
+
+
+
+  /* ==========================================================================
+    Form Ajax
+  =========================================================================== */
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("contact-form");
+    var button = document.getElementById("contact-submit");
+    var status = document.getElementById("form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks! Your message was sent successfully.";
+    }
+
+    function error(response) {
+      status.innerHTML = "Oops! There was a problem: " + JSON.parse(response).error;
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.response);
+        // console.log(xhr);
+      }
+    };
+    xhr.send(data);
+  }
